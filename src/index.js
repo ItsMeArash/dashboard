032930ed85d3da5c000cd4daf741235a07b864ae
Arash Harandehi <arashh7998@gmail.com>
2023-12-23 16:16:08 +0330

    sidebar completed

--- src/index.js
+++ src/index.js
@@ -4,4 +4,11 @@ import ReactDOM from "react-dom";
 import App from "./App.js";
 import "./index.css";
 
-ReactDOM.render(<App />, document.getElementById("root"));
+import { ContextProvider } from "./context/ContextProvider.js";
+
+ReactDOM.render(
+    <ContextProvider>
+        <App />
+    </ContextProvider>,
+    document.getElementById("root")
+);
